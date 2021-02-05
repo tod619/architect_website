156741ed9c47a8d074c26ead2b7cb79c3932401c
tod619 <trevorodwyer619@hotmail.com>
2021-02-05 15:37:46 +0000

    add js script to hide circles + display site in script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Hide spinner + display page
+window.onload = () => {
+    setTimeout(() =>{
+        document.querySelector("body").classList.add("display")
+    },4000)
+}
+
 // Show hide sideMenu
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
     document.querySelector(".container").classList.toggle("change");
